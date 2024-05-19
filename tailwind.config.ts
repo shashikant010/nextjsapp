fffca98c8e8ddb54e36781d01f5c5b506dbdf6be
shashikant010 <shashikantyadav0503@gmail.com>
2024-05-20 00:47:24 +0530

    fixed some deployment issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,24 +8,6 @@ const {
  
 
 const config: Config = {
-  theme: {
-    extend: {
-      animation: {
-        "meteor-effect": "meteor 5s linear infinite",
-      },
-      keyframes: {
-        meteor: {
-          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
-          "70%": { opacity: "1" },
-          "100%": {
-            transform: "rotate(215deg) translateX(-500px)",
-            opacity: "0",
-          },
-        },
-      },
-    },
- 
-},
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
